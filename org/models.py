e95517c9007d32fbae7a067dc0268f18ed54af4f
troyand <troyanovsky@gmail.com>
2011-11-26 15:40:26 +0200

    University model

--- org/models.py
+++ org/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class University(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    abbr = models.CharField(max_length=16, unique=True)
+
+    def __unicode__(self):
+        return u'%s' % (
+                self.abbr
+                )
