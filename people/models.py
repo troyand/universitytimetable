f356cd370d599d052b99148104aac493a9971588
troyand <troyanovsky@gmail.com>
2011-12-23 15:45:03 +0200

    LecturerStatus model

--- people/models.py
+++ people/models.py
@@ -1,4 +1,7 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
+from universitytimetable.org.models import Department
 
 class Person(models.Model):
     first_name = models.CharField(max_length=64, null=True, blank=True)
@@ -11,3 +14,19 @@ class Person(models.Model):
                 self.first_name[0],
                 self.middle_name[0],
                 )
+
+
+LECTURER_STATUS_CHOICES = (
+        (u'ас.', u'асистент'),
+        (u'ст. викл.', u'старший викладач'),
+        (u'доц.', u'доцент'),
+        (u'проф.', u'професор'),
+        )
+
+
+class LecturerStatus(models.Model):
+    person = models.ForeignKey(Person)
+    department = models.ForeignKey(Department, null=True, blank=True)
+    status = models.CharField(max_length=255, choices=LECTURER_STATUS_CHOICES)
+    date_from = models.DateField()
+    date_to = models.DateField(null=True, blank=True)
